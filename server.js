2b4b36722d22c607ccff89ef3f3f00c8008c3cca
damaren <damarenster@gmail.com>
2015-11-09 21:56:52 +0100

    first working commit

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express');
 var app = express();
 var path = require('path')
 var fs = require('fs');
-var bodyParser = require('body-parser');
-var uuid = require('node-uuid');
 
 app.set('view engine', 'jade');
 //app.use(express.static(path.join(__dirname, 'public')));
@@ -13,13 +11,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
-
-app.use(bodyParser());
-
-app.get('/newId', function (req, res) {
-  console.log("sending new ID");
-  res.send(uuid.v4());
 });
 
 app.get('/*', function (req, res) {
