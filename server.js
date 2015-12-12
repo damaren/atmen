38ebcf298100089f543a5630d8069ebff8dbc44e
damaren <damarenster@gmail.com>
2015-12-12 23:15:45 +0100

    added the default paths (home, about, message, gallery) to the server

--- server.js
+++ server.js
@@ -7,10 +7,16 @@ app.set('view engine', 'jade');
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('public'));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.get('/message', function (req, res) {
+  res.redirect('/messages.html');
+});
+
+app.get('/gallery', function (req, res) {
+  res.redirect('/gallery.html');
+});
+
+app.get('/about', function (req, res) {
+  res.redirect('/about.html');
 });
 
 app.get('/*', function (req, res) {
@@ -21,8 +27,6 @@ var server = app.listen(3000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
-
-  
 
   console.log('Example app listening at http://%s:%s', host, port);
 
